74980cb2710a2f7e8953fa1eea6fcc37a58c71f9
Jhon Baron <32346414+Jhooomn@users.noreply.github.com>
2021-07-27 07:07:14 -0500

    Update TestingActionsSpringBootJava8ApplicationTests.java

--- src/test/java/com/jhoomn/testingactionsspringbootjava8/TestingActionsSpringBootJava8ApplicationTests.java
+++ src/test/java/com/jhoomn/testingactionsspringbootjava8/TestingActionsSpringBootJava8ApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 class TestingActionsSpringBootJava8ApplicationTests {
 
     private static final String A_VALUE = "A";
-    private static final String B_VALUE = "A";
+    private static final String B_VALUE = "C";
 
     @Test
     void justASimpleTest() throws Exception {
