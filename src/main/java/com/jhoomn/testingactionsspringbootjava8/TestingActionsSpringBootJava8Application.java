2a5eca7fdc68381b1fa3ea997623b2f057913b5c
Jhon Camilo Baron Berdugo <jbaron@mutualser.org>
2021-07-27 06:48:58 -0500

    first commit

---
+++ src/main/java/com/jhoomn/testingactionsspringbootjava8/TestingActionsSpringBootJava8Application.java
@@ -0,0 +1,13 @@
+package com.jhoomn.testingactionsspringbootjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingActionsSpringBootJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingActionsSpringBootJava8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhoomn/testingactionsspringbootjava8/TestingActionsSpringBootJava8ApplicationTests.java
@@ -0,0 +1,214 @@
+package com.jhoomn.testingactionsspringbootjava8;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+class TestingActionsSpringBootJava8ApplicationTests {
+
+    private static final String A_VALUE = "A";
+    private static final String B_VALUE = "A";
+
+    @Test
+    void justASimpleTest() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest2() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest3() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest4() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest5() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest6() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest7() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest8() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest9() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest10() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest11() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest12() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest13() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest14() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest15() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest16() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest17() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest18() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest19() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest20() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest21() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest22() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest23() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest24() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest25() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest26() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest27() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest28() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest29() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest30() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest31() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest32() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest33() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest34() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest35() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest36() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest37() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest38() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest39() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+
+    @Test
+    void justASimpleTest40() throws Exception {
+        assertEquals(A_VALUE, B_VALUE);
+    }
+}
